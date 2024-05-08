50bc923f84cc449452c1573b2b6f8fa482b8f460
Gál Ádám <galadam96@gmail.com>
2024-05-08 11:36:27 +0200

    Add files via upload

---
+++ src/equalSumChecker.java
@@ -0,0 +1,13 @@
+public class equalSumChecker {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasEqualSum(int x, int y, int z){
+        boolean result = false;
+        if((x + y) == z){
+            result = true;
+        }
+        return result;
+    }
+}
